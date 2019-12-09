f2d98dfd6b519e2d9f3ed5eba2f603d728563ea7
Yazan Arafeh <yazanarafeh24@gmail.com>
2019-12-08 21:20:02 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Yazan Arafeh on 12/8/19.
+//  Copyright © 2019 Arafeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
